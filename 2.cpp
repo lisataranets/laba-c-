4570e4901bfad7ac54fed8464d5853d3d7630e72
lisa <150896662+lisataranets@users.noreply.github.com>
2025-03-13 07:36:06 +0300

    laba3

--- 2.cpp
+++ 2.cpp
@@ -1,72 +1,127 @@
 ﻿#include <iostream>
-#include <set>
-#include <utility> 
+#include <cstdlib> 
+#include <ctime>  
 #include <string>
-#include <limits>  
-
 using namespace std;
+struct PointerInfo {
+    void* ptr;   
+    size_t size;  
+};
 
-void printset(const set<pair<char, char>>& result_set) {
-    int count = 1;
-    for (const auto& element : result_set) {
-        cout << count++ << ": (" << element.first << ", " << element.second << ")\n";
+const int MAX_SEGMENT_SIZE = 100;
+PointerInfo staticSegment[MAX_SEGMENT_SIZE];
+int segmentSize = 0; 
+
+void* NewPointer(size_t byte) {
+    if (segmentSize >= MAX_SEGMENT_SIZE) {
+        cout << "Ошибка: статический сегмент переполнен!" << endl;
+        return nullptr;
     }
+
+    void* ptr = malloc(byte); 
+    if (ptr == nullptr) {
+        cout << "Ошибка: не удалось выделить память!" << endl;
+        return nullptr;
+    }
+
+    staticSegment[segmentSize].ptr = ptr;
+    staticSegment[segmentSize].size = byte;
+    segmentSize++;
+
+    cout << "Память успешно выделена и сохранена в сегменте." << endl;
+    return ptr;
+}
+
+template <typename T>
+void WritePointer(T* p, T value) {
+    if (p == nullptr) {
+        cout << "Ошибка: указатель равен null!" << endl;
+        return;
+    }
+    *p = value;
+}
+
+template <typename T>
+T ReadPointer(T* p) {
+    if (p == nullptr) {
+        cout << "Ошибка: указатель равен null!" << endl;
+        return T();
+    }
+    return *p;
+}
+
+template <typename T>
+void SetPointer(T* p, T* b) {
+    if (p == nullptr || b == nullptr) {
+        cout << "Ошибка: один из указателей равен null!" << endl;
+        return;
+    }
+    *p = *b;
+}
+
+void FreePointer(void* ptr) {
+    for (int i = 0; i < segmentSize; i++) {
+        if (staticSegment[i].ptr == ptr) {
+            free(ptr);
+            staticSegment[i].ptr = nullptr; 
+            staticSegment[i].size = 0; 
+            cout << "Память успешно освобождена." << endl;
+            return;
+        }
+    }
+    cout << "Ошибка: указатель не найден в сегменте!" << endl;
+}
+
+int countMultiplesOfThree(int* arr, int n) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (*(arr + i) % 3 == 0) {
+            count++;
+        }
+    }
+    return count;
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    set<char> A, B;
-    char input;
-    string n, m;
+    srand(time(0));
+    string n;
     while (true) {
-        cout << "Введите количество элементов множества A: ";
+        cout << "Введите размер массива: ";
         getline(cin, n);
         if (n.empty() || n.find_first_not_of("0123456789") != string::npos) {
             cout << "Ошибка! Введите число" << endl;
             continue;
         }
-        break;
-    }
-    int numA = stoi(n);
-    cout << "Введите элементы множества A (по одному символу):\n";
-    for (int i = 0; i < numA; ++i) {
-        cin >> input;
-        A.insert(input);
-    }
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-    while (true) {
-        cout << "Введите количество элементов множества B: ";
-        getline(cin, m);
-        if (m.empty() || m.find_first_not_of("0123456789") != string::npos) {
-            cout << "Ошибка! Введите число" << endl;
+        int number = stoi(n);
+        if (number <= 0) {
+            cout << "Ошибка! Введите положительное число." << endl;
             continue;
         }
         break;
     }
-    int numB = stoi(m);
-    cout << "Введите элементы множества B (по одному символу):\n";
-    for (int i = 0; i < numB; ++i) {
-        cin >> input;
-        B.insert(input);
+
+    int arraySize = stoi(n); 
+
+    int* A = (int*)NewPointer(arraySize * sizeof(int));
+    if (A == nullptr) {
+        return 1;
     }
 
-    set<pair<char, char>> A_cross_B;
-    set<pair<char, char>> B_cross_A;
+    for (int i = 0; i < arraySize; i++) {
+        WritePointer(A + i, rand() % 100);
+    }
 
-    for (const auto& a : A) {
-        for (const auto& b : B) {
-            A_cross_B.insert(make_pair(a, b));
-        }
+    cout << "Массив: ";
+    for (int i = 0; i < arraySize; i++) {
+        cout << ReadPointer(A + i) << " ";
     }
-    for (const auto& b : B) {
-        for (const auto& a : A) {
-            B_cross_A.insert(make_pair(b, a));
-        }
-    }
-    cout << "A x B:\n";
-    printset(A_cross_B);
-    cout << "B x A:\n";
-    printset(B_cross_A);
+    cout << endl;
+
+    int count = countMultiplesOfThree(A, arraySize);
+    cout << "Количество элементов, кратных 3: " << count << endl;
+
+    FreePointer(A);
+
     return 0;
 }
